b31158418ce1681b520921a609ced5378fc1c3d5
homePC <aymkin@gmail.com>
2019-06-05 10:13:41 +0300

    convert SPEED() to getRandSpeed() and make it as Enemy prototype

--- js/app.js
+++ js/app.js
@@ -14,30 +14,30 @@ const FIELD = {
 	bottomBorder: 322,
 };
 
-const SPEED = (min = 75, max = 200) =>
-	Math.floor(Math.random() * (max - min + 1)) + min;
-
-const Creature = function (x, y) {
+const Creature = function(x, y) {
 	this.x = x;
 	this.y = y;
 };
 
-
-	const Enemy = function(x, y, speed, gamer) {
+const Enemy = function(x, y, gamer) {
 	Creature.call(this, x, y);
-	this.speed = speed;
+	this.speed = this.getRandSpeed();
 	this.gamer = gamer;
 	this.sprite = 'images/enemy-bug.png';
+};
+
+Enemy.prototype.getRandSpeed = function(min = 75, max = 200) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 Enemy.prototype.update = function(dt) {
 	this.x = this.x + this.speed * dt;
 	if (this.x >= FIELD.rightBorder + FIELD.column * 2) {
 		this.x = -101;
-		this.speed = SPEED();
+		this.speed = this.getRandSpeed();
 	}
 	if (this.isCollision()) {
-		alert(`You lost! The score is demotioned to ${scoreCount -= 1}`);
+		alert(`You lost! The score is demotioned to ${(scoreCount -= 1)}`);
 		this.gamer.beginAgain();
 	}
 };
@@ -85,14 +85,13 @@ Player.prototype.handleInput = function(key) {
 
 			if (this.y < 0) {
 				scoreCount++;
-				if (scoreCount < 5){
+				if (scoreCount < 5) {
 					alert(`You win ${scoreCount} times!`);
 				} else {
 					alert(`You are absolute winner with score ${scoreCount} !`);
 					scoreCount = 0;
 					alert('The new game is started');
 				}
-				
 				this.beginAgain();
 			}
 			break;
@@ -103,9 +102,9 @@ Player.prototype.handleInput = function(key) {
 };
 
 const player = new Player(START.x, START.y);
-const enemy1 = new Enemy(0, FIELD.row - 20, SPEED(), player);
-const enemy2 = new Enemy(0, FIELD.row * 2 - 20, SPEED(), player);
-const enemy3 = new Enemy(0, FIELD.row * 3 - 20, SPEED(), player);
+const enemy1 = new Enemy(0, FIELD.row - 20, player);
+const enemy2 = new Enemy(0, FIELD.row * 2 - 20, player);
+const enemy3 = new Enemy(0, FIELD.row * 3 - 20, player);
 
 const allEnemies = [enemy1, enemy2, enemy3];
 
